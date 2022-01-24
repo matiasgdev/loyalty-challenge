1526d06e5ae36933ac48571a31c976397f24a8e3
Matias Navarro <matiasg.developer@gmail.com>
2022-01-23 23:36:21 -0300

    add a misc folder with walkthrough items

---
+++ misc/walkthrough-items.ts
@@ -0,0 +1,26 @@
+export const walkthroughItems = [
+  {
+    id: 1,
+    title: 'Browse',
+    description: 'Browse our tech catalog with more than 20 top tech products',
+    rotation: '-3deg',
+    illustrationUrl: '/assets/illustrations/walkthroug-1-desktop.png',
+    iconUrl: '/assets/icons/walkthrough-1.png',
+  },
+  {
+    id: 2,
+    title: 'Chosee',
+    description: 'Exchange your hard earned AeroPoints for the item you want',
+    rotation: '0deg',
+    illustrationUrl: '/assets/illustrations/walkthroug-2-desktop.png',
+    iconUrl: '/assets/icons/walkthrough-2.png',
+  },
+  {
+    id: 3,
+    title: 'Enjoy',
+    description: "All done, you can relad! We'll take care of delivery of your tech item!",
+    rotation: '3deg',
+    illustrationUrl: '/assets/illustrations/walkthroug-3-desktop.png',
+    iconUrl: '/assets/icons/walkthrough-3.png',
+  },
+];
