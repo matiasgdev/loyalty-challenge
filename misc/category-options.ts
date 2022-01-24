859b9d11a1e468c3e8ad028898db5a3190aba561
Matias Navarro <matiasg.developer@gmail.com>
2022-01-24 00:06:05 -0300

    adding options to a reusable confg'

---
+++ misc/category-options.ts
@@ -0,0 +1,22 @@
+export const categoryOptions = [
+  {
+    value: 'all',
+    name: 'All products',
+  },
+  {
+    value: 'gaming',
+    name: 'Gaming',
+  },
+  {
+    value: 'audio',
+    name: 'Audio',
+  },
+  {
+    value: 'smart',
+    name: 'Smart Home',
+  },
+  {
+    value: 'tv',
+    name: 'Monitors & TV',
+  },
+];
---
+++ misc/sort-by-options.ts
@@ -0,0 +1,14 @@
+export const sortByOptions = [
+  {
+    value: 'recent',
+    title: 'Most Recent',
+  },
+  {
+    value: 'lowest',
+    title: 'Lowest Price',
+  },
+  {
+    value: 'highest',
+    title: 'Highest Price',
+  },
+];
